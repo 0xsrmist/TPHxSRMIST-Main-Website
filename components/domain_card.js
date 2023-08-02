25972041b20669a626611948922ea203a0cec1b3
Abhishek Yadav <abhishekaug2002@gmail.com>
2023-08-02 17:19:31 +0530

    domain section completed

---
+++ components/domain_card.js
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+const Domain_card = ({ image,Title, Desc }) => {
+    return (
+        <div className="flex flex-col gap-6 w-72 text-white">
+            <div className="p-3 rounded-lg bg-[#130F15]">
+                <Image src={image} alt="domain" width={500} height={600} objectFit="cover"/>
+            </div>
+            <div className="flex flex-col items-center text-center gap-2">
+                <h2 className="flex text-2xl font-medium">{Title}</h2>
+                <span>{Desc}</span>
+            </div>
+        </div>
+    );
+}
+
+export default Domain_card;
---
+++ components/layouts/domains.js
@@ -0,0 +1,16 @@
+import Domain_card from '../domain_card'
+
+const Domains = () => {
+    return (
+        <div className="flex flex-col gap-8 px-16 py-6">
+            <h2 className="flex justify-center text-white text-5xl font-medium">DOMAINS</h2>
+            <div className="flex flex-col md:flex-row gap-6 gap-y-20 justify-between items-center">
+                <Domain_card image="./assets/images//technical.svg" Title="TECHNICAL" Desc="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam voluptate omnis assumenda"/>
+                <Domain_card image="./assets/images/creative.svg" Title="CREATIVE" Desc="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam voluptate omnis assumenda"/>
+                <Domain_card image="./assets/images/corporate.svg" Title="CORPORATE" Desc="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam voluptate omnis assumenda"/>
+            </div>
+        </div>
+    );
+}
+
+export default Domains;
