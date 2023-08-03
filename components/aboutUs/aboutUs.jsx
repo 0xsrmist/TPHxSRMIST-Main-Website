d097f97b3b546ed530d2031247489ef3744e5db0
Abhishek Yadav <abhishekaug2002@gmail.com>
2023-08-03 23:05:59 +0530

    aboutus svg changed

--- components/aboutUs/aboutUs.jsx
+++ components/aboutUs/aboutUs.jsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import AboutUsImage from "../../public/assets/images/aboutImage.svg";
+import AboutUsImage from "../../public/assets/images/about.svg";
 
 export default function AboutUs() {
   return (
@@ -7,31 +7,40 @@ export default function AboutUs() {
       <div>
         {" "}
         <h2 className="lg:text-5xl text-4xl font-medium text-center text-white">
-          About Us
+          ABOUT US
         </h2>
       </div>
-      <div className="container mx-auto flex flex-col items-center px-2 py-24 md:flex-row">
-        <div className="mb-8 flex w-4/5 md:mb-0 md:w-1/2 lg:w-full lg:max-w-lg">
+      <div className="container mx-auto flex flex-col justify-center items-center px-2 py-24 md:flex-row">
+        <div className="flex justify-center items-center">
           <Image
-            className=" object-cover"
+            objectFit="cover"
             alt="hero"
             src={AboutUsImage}
-            width={600}
-            height={600}
+            width={500}
+            height={500}
           />
         </div>
-        <div className="flex flex-col items-center text-center md:w-1/2 md:items-start md:pl-16 md:text-left lg:flex-grow lg:pl-24">
+        <div className="flex flex-col items-center text-center md:w-1/2 md:items-start md:pl-16 md:text-left lg:flex-grow lg:pl-24 p-8">
           <p className="mb-8 text-white leading-relaxed text-justify text-lg ">
             At vero eos et accusamus et iusto odio dignissimos ducimus qui
             blanditiis praesentium voluptatum deleniti atque corrupti quos
             dolores et quas molestias excepturi sint occaecati cupiditate non
-            provident. At vero eos et accusamus et iusto odio dignissimos
+            provident.
+          </p>
+          <p className="mb-8 text-white leading-relaxed text-justify text-lg ">
+            At vero eos et accusamus et iusto odio dignissimos
             ducimus qui blanditiis praesentium voluptatum deleniti atque
             corrupti quos dolores et quas molestias excepturi sint occaecati
             cupiditate non provident. At vero eos et accusamus et iusto odio
             dignissimos ducimus qui blanditiis praesentium voluptatum deleniti
             atque corrupti quos dolores et quas molestias excepturi sint
             occaecati cupiditate non provident.
+          </p>
+          <p className="mb-8 text-white leading-relaxed text-justify text-lg ">
+            At vero eos et accusamus et iusto odio dignissimos
+            ducimus qui blanditiis praesentium voluptatum deleniti atque
+            corrupti quos dolores et quas molestias excepturi sint occaecati
+            cupiditate non provident.
           </p>
         </div>
       </div>
