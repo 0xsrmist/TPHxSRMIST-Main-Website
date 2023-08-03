a8ba6af9e13ff1aa9d2be81a53c0f3446e3c0d30
Om <ommukherjee28@gmail.com>
2023-08-03 22:29:34 +0530

    updating sponsor component into page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Domains from "../components/layouts/domains";
-import Footer from "@/components/layouts/Footer"
+import Footer from "@/components/layouts/Footer";
+import Sponsor from "../components/layouts/Sponsor";
 
 export default function Home() {
   return (
     <main className="bg-black text-white">
+      <Sponsor />
       <Domains />
       <Footer />
     </main>
