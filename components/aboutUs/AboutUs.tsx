691403306e7868ea6efacad24bdf04ca7720fe32
Kunal Keshan <kunalkeshan12@gmail.com>
2023-08-27 18:42:27 +0530

    chore: added radial gradient

--- components/aboutUs/AboutUs.tsx
+++ components/aboutUs/AboutUs.tsx
@@ -11,14 +11,16 @@ export default function AboutUs() {
 				</h2>
 			</div>
 			<div className='container mx-auto flex flex-col justify-center items-center px-2 py-24 md:flex-row'>
-				<div className='flex justify-center items-center'>
+				<div className='flex justify-center items-center relative'>
 					<Image
 						objectFit='cover'
 						alt='hero'
 						src={AboutUsImage}
 						width={500}
 						height={500}
+						className='z-10'
 					/>
+					<div className='absolute z-[5] -bottom-20 -right-40 w-72 h-72 bg-[#BB2CFF] rounded-full filter blur-[100px] opacity-70'></div>
 				</div>
 				<div className='flex flex-col items-center text-center md:w-1/2 md:items-start md:pl-16 md:text-left lg:flex-grow lg:pl-24 p-8'>
 					<p className='mb-8 text-white leading-relaxed text-justify text-lg '>
