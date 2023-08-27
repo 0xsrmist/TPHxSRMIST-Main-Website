0da21ca990f3e7154072f197bb0dcdc6142a05d3
Kunal Keshan <kunalkeshan12@gmail.com>
2023-08-27 18:22:52 +0530

    chore: added og and twitter meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,19 @@ const helvetica = localFont({
 export const metadata: Metadata = {
 	title: 'TPH x SRMIST | Home',
 	description: 'Official Website of TPH x SRMIST.',
+	openGraph: {
+		type: 'website',
+		title: 'TPH x SRMIST | Home',
+		description: 'Official Website of TPH x SRMIST.',
+		url: 'https://tph-srmist.vercel.app',
+		images: ['/og-thumbnail.png'],
+	},
+	twitter: {
+		title: 'TPH x SRMIST | Home',
+		description: 'Official Website of TPH x SRMIST.',
+		card: 'summary_large_image',
+		images: ['/twitter-thumbnail.png'],
+	},
 };
 
 export default function RootLayout({
